40790d5e729f70868026872500ecf297e4ebf269
drolya <drolya@chemaxon.com>
2015-11-15 18:23:07 +0100

    feat(transactions): Basics of manual transactions creation and listing, very basics

--- packages/custom/myby/public/controllers/myby.js
+++ packages/custom/myby/public/controllers/myby.js
@@ -1,11 +1,27 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.myby').controller('MybyController', ['$scope', 'Global', 'Myby',
-  function($scope, Global, Myby) {
-    $scope.global = Global;
-    $scope.package = {
-      name: 'myby'
+angular.module('mean.myby').controller('MybyController', ['$scope', 'Global', 'Transactions',
+  function($scope, Global, Transactions) {
+    var vm = this;
+
+    vm.global = Global;
+    vm.transactions = [];
+
+    Transactions.all(function(transactions) {
+      vm.transactions = transactions;
+    });
+
+    vm.create = function(valid) {
+      if (!valid) return;
+
+      var transaction = new Transactions(vm.transaction);
+      transaction.$save(function(response) {
+        vm.transactions.push(transaction.name);
+        vm.transaction = {};
+      }, function(err) {
+        alert('Cannot register transaction');
+      });
     };
   }
 ]);
--- packages/custom/myby/public/routes/myby.js
+++ packages/custom/myby/public/routes/myby.js
@@ -2,9 +2,14 @@
 
 angular.module('mean.myby').config(['$stateProvider',
   function($stateProvider) {
-    $stateProvider.state('myby example page', {
-      url: '/transactions',
-      templateUrl: 'myby/views/index.html'
-    });
+    $stateProvider
+        .state('myby example page', {
+        url: '/transactions',
+        templateUrl: 'myby/views/index.html'
+      })
+      .state('create transaction', {
+        url: '/transactions/create',
+        templateUrl: 'myby/views/create.html'
+      });
   }
 ]);
