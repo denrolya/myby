184ac5d35d3a3e19da7799e3808335c04b25da25
drolya <drolya@chemaxon.com>
2015-11-17 22:55:15 +0100

    feat(pagination): Added bootstrap's pagination directive

--- packages/custom/myby/public/services/myby.js
+++ packages/custom/myby/public/services/myby.js
@@ -2,9 +2,7 @@
 
 angular.module('mean.myby').factory('Transactions', ['$resource',
   function($resource) {
-    return $resource('api/transactions/:name', {
-      name: '@name'
-    }, {
+    return $resource('api/transactions/:name', { name: '@name' }, {
       update: {
         method: 'PUT'
       },
@@ -16,7 +14,12 @@ angular.module('mean.myby').factory('Transactions', ['$resource',
       all: {
         method: 'GET',
         isArray: true,
-        url: '/api/transactions'
+        url: '/api/transactions?perPage=:perPage&pageNum=:pageNum',
+      },
+      count: {
+        method: 'GET',
+        isArray: false,
+        url: '/api/transactions/count'
       }
     });
   }
